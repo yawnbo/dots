88ba03e1b2f2f93a1617fdae9d690df2ed16f155
yawnbo <ownuniverse@yahoo.com>
2024-09-08 23:19:04 -0700

    copilot setup

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -68,6 +68,14 @@ end)
 require("lazy").setup({
 	{ "rebelot/kanagawa.nvim", lazy = false, priority = 1000 },
 	{ "tpope/vim-sleuth" },
+	--{ "github/copilot.vim" },
+	{ "zbirenbaum/copilot.lua", cmd = "Copilot", event = "InsertEnter", lazy = true },
+	{
+		"zbirenbaum/copilot-cmp",
+		config = function()
+			require("copilot_cmp").setup()
+		end,
+	},
 	{
 		"lewis6991/gitsigns.nvim",
 		opts = {
@@ -725,6 +733,50 @@ require("nvim-treesitter.configs").setup({
 		enable = true,
 		additional_vim_regex_highlighting = false,
 	},
+})
+require("copilot").setup({
+	panel = {
+		enabled = true,
+		auto_refresh = false,
+		keymap = {
+			jump_prev = "[[",
+			jump_next = "]]",
+			accept = "<CR>",
+			refresh = "gr",
+			open = "<M-CR>",
+		},
+		layout = {
+			position = "bottom", -- | top | left | right
+			ratio = 0.4,
+		},
+	},
+	suggestion = {
+		enabled = true,
+		auto_trigger = false,
+		hide_during_completion = true,
+		debounce = 75,
+		keymap = {
+			accept = "<M-l>",
+			accept_word = false,
+			accept_line = false,
+			next = "<M-]>",
+			prev = "<M-[>",
+			dismiss = "<C-]>",
+		},
+	},
+	filetypes = {
+		yaml = false,
+		markdown = false,
+		help = false,
+		gitcommit = false,
+		gitrebase = false,
+		hgcommit = false,
+		svn = false,
+		cvs = false,
+		["."] = false,
+	},
+	copilot_node_command = "node", -- Node.js version must be > 18.x
+	server_opts_overrides = {},
 })
 -- set colorscheme for overall vim
 vim.cmd("colorscheme kanagawa")
