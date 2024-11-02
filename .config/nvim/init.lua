438628321f8c8b223172987bc95f273efa9a9196
Yan Bogdanovskyy <132106589+yawnbo@users.noreply.github.com>
2024-11-01 17:20:34 -0700

    nvim themes

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -78,7 +78,12 @@ end)
 
 -- lazy init list of plugs
 require("lazy").setup({
+	--themes setup
 	{ "rebelot/kanagawa.nvim", lazy = false, priority = 1000 },
+	{ "navarasu/onedark.nvim", priority = 1000 },
+
+	-- end themes
+
 	{ "akinsho/bufferline.nvim", version = "*", dependencies = "nvim-tree/nvim-web-devicons" },
 	{ "tpope/vim-sleuth" },
 	{
@@ -93,12 +98,11 @@ require("lazy").setup({
 		end,
 	},
 	{
-		"altermo/ultimate-autopair.nvim",
-		event = { "InsertEnter", "CmdlineEnter" },
-		branch = "v0.6", --recommended as each new version will have breaking changes
-		opts = {
-			--Config goes here
-		},
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		config = true,
+		-- use opts = {} for passing setup options
+		-- this is equivalent to setup({}) function
 	},
 	--{ "github/copilot.vim" },
 	{ "mhinz/vim-startify" },
@@ -693,6 +697,46 @@ require("lazy").setup({
 })
 
 -- setup of individual plugins begin here
+-- Lua
+require("onedark").setup({
+	-- Main options --
+	style = "dark", -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
+	transparent = false, -- Show/hide background
+	term_colors = true, -- Change terminal color as per the selected theme style
+	ending_tildes = false, -- Show the end-of-buffer tildes. By default they are hidden
+	cmp_itemkind_reverse = false, -- reverse item kind highlights in cmp menu
+
+	-- toggle theme style ---
+	toggle_style_key = nil, -- keybind to toggle theme style. Leave it nil to disable it, or set it to a string, for example "<leader>ts"
+	toggle_style_list = { "dark", "darker", "cool", "deep", "warm", "warmer", "light" }, -- List of styles to toggle between
+
+	-- Change code style ---
+	-- Options are italic, bold, underline, none
+	-- You can configure multiple style with comma separated, For e.g., keywords = 'italic,bold'
+	code_style = {
+		comments = "italic",
+		keywords = "none",
+		functions = "none",
+		strings = "none",
+		variables = "none",
+	},
+
+	-- Lualine options --
+	lualine = {
+		transparent = false, -- lualine center bar transparency
+	},
+
+	-- Custom Highlights --
+	colors = {}, -- Override default colors
+	highlights = {}, -- Override highlight groups
+
+	-- Plugins Config --
+	diagnostics = {
+		darker = true, -- darker colors for diagnostic
+		undercurl = true, -- use undercurl instead of underline for diagnostics
+		background = true, -- use background color for virtual text
+	},
+})
 require("kanagawa").setup({
 	compile = false, -- enable compiling the colorscheme
 	undercurl = true, -- enable undercurls
@@ -711,20 +755,10 @@ require("kanagawa").setup({
 	overrides = function(colors) -- add/modify highlights
 		return {}
 	end,
-	theme = "wave", -- Load "wave" theme when 'background' option is not set
+	theme = "dragon", -- Load "wave" theme when 'background' option is not set
 	background = { -- map the value of 'background' option to a theme
-		dark = "wave", -- try "dragon" !
+		dark = "dragon", -- try "dragon" !
 		light = "lotus",
-	},
-})
--- Treesitter init
-require("nvim-treesitter.configs").setup({
-	ensure_installed = { "c", "lua", "html" },
-	sync_install = false,
-
-	highlight = {
-		enable = true,
-		additional_vim_regex_highlighting = false,
 	},
 })
 require("copilot").setup({
@@ -791,4 +825,4 @@ require("bufferline").setup({
 	},
 })
 vim.cmd("AirlineTheme deus")
-vim.cmd("colorscheme kanagawa")
+vim.cmd("colorscheme onedark")
