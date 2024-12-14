8d46dc07c5506ced2138f93d29721df5c4fc1de2
yawnbo <ownuniverse@yahoo.com>
2024-12-14 15:41:06 -0800

    structured nvim2

---
+++ .config/nvim/lua/opts.lua
@@ -0,0 +1,98 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+vim.g.have_nerd_font = true
+vim.opt.termguicolors = true
+vim.g.airline_extensions_tabline_enabled = 0
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- opts
+vim.o.wildmenu = true
+vim.o.wildmode = "list:longest,list:full"
+vim.opt.showbreak = "↳"
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.wildignore:append({ ".javac", "node_modules", "*.pyc" })
+vim.opt.wildignore:append({ ".aux", ".out", ".toc" })
+vim.opt.wildignore:append({
+	".o",
+	".obj",
+	".dll",
+	".exe",
+	".so",
+	".a",
+	".lib",
+	".pyc",
+	".pyo",
+	".pyd",
+	".swp",
+	".swo",
+	".class",
+	".DS_Store",
+	".git",
+	".hg",
+	".orig",
+})
+vim.opt.number = true
+vim.opt.mouse = "a"
+vim.opt.showmode = false
+vim.opt.breakindent = true
+vim.opt.undofile = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+--relative nums
+vim.opt.relativenumber = true
+vim.opt.signcolumn = "yes"
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 300
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+vim.opt.list = true
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+vim.opt.inccommand = "split"
+vim.opt.cursorline = true
+vim.opt.scrolloff = 10
+vim.o.tabstop = 3
+vim.o.shiftwidth = 2
+
+-- latex dont know if needed or not
+vim.cmd("filetype plugin on")
+-- keybinds for buffers
+vim.api.nvim_set_keymap("n", "<leader>bn", ":bnext<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>bp", ":bprevious<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>b", ":buffers<CR>:b ", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>bd", ":bd<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>bo", ":tabonly<CR>", { noremap = true, silent = true })
+
+vim.g.airline_powerline_fonts = 1
+
+--clear highlight on search in norm
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+--term mode exits
+vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+--window focusing with hjkl
+vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+--vim.keymap.set("n", "leader-t",
+--highlight yanks
+vim.api.nvim_create_autocmd("TextYankPost", {
+	group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
+
+-- sync clipboard
+vim.schedule(function()
+	vim.opt.clipboard = "unnamedplus"
+end)
+
+vim.api.nvim_create_augroup("AutoSaveTex", { clear = true })
+
+vim.api.nvim_create_autocmd("InsertLeave", {
+	group = "AutoSaveTex",
+	pattern = "*.tex",
+	command = "write",
+})
---
+++ .config/nvim/lua/plugins/setups.lua
@@ -0,0 +1,133 @@
+require("onedark").setup({
+	-- Main options --
+	style = "dark", -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
+	transparent = false, -- Show/hide background
+	term_colors = true, -- Change terminal color as per the selected theme style
+	ending_tildes = false, -- Show the end-of-buffer tildes. By default they are hidden
+	cmp_itemkind_reverse = false, -- reverse item kind highlights in cmp menu
+
+	-- toggle theme style ---
+	toggle_style_key = nil, -- keybind to toggle theme style. Leave it nil to disable it, or set it to a string, for example "<leader>ts"
+	toggle_style_list = { "dark", "darker", "cool", "deep", "warm", "warmer", "light" }, -- List of styles to toggle between
+
+	-- Change code style ---
+	-- Options are italic, bold, underline, none
+	-- You can configure multiple style with comma separated, For e.g., keywords = 'italic,bold'
+	code_style = {
+		comments = "italic",
+		keywords = "none",
+		functions = "none",
+		strings = "none",
+		variables = "none",
+	},
+
+	-- Lualine options --
+	lualine = {
+		transparent = false, -- lualine center bar transparency
+	},
+
+	-- Custom Highlights --
+	colors = {
+		bg0 = "#282a36",
+	}, -- Override default colors
+	highlights = {}, -- Override highlight groups
+
+	-- Plugins Config --
+	diagnostics = {
+		darker = true, -- darker colors for diagnostic
+		undercurl = true, -- use undercurl instead of underline for diagnostics
+		background = true, -- use background color for virtual text
+	},
+})
+require("kanagawa").setup({
+	compile = false, -- enable compiling the colorscheme
+	undercurl = true, -- enable undercurls
+	commentStyle = { italic = true },
+	functionStyle = {},
+	keywordStyle = { italic = true },
+	statementStyle = { bold = true },
+	typeStyle = {},
+	transparent = false, -- do not set background color
+	dimInactive = false, -- dim inactive window `:h hl-NormalNC`
+	terminalColors = true, -- define vim.g.terminal_color_{0,17}
+	colors = { -- add/modify theme and palette colors
+		palette = {},
+		theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+	},
+	overrides = function(colors) -- add/modify highlights
+		return {}
+	end,
+	theme = "dragon", -- Load "wave" theme when 'background' option is not set
+	background = { -- map the value of 'background' option to a theme
+		dark = "dragon", -- try "dragon" !
+		light = "lotus",
+	},
+})
+require("nvim-autopairs").setup({
+	disable_filetype = { "tex" },
+})
+require("copilot").setup({
+	panel = {
+		enabled = true,
+		auto_refresh = false,
+		keymap = {
+			jump_prev = "[[",
+			jump_next = "]]",
+			accept = "<CR>",
+			refresh = "gr",
+			open = "<M-CR>",
+		},
+		layout = {
+			position = "bottom", -- | top | left | right
+			ratio = 0.4,
+		},
+	},
+	suggestion = {
+		enabled = true,
+		auto_trigger = false,
+		hide_during_completion = true,
+		debounce = 75,
+		keymap = {
+			accept = "<M-l>",
+			accept_word = false,
+			accept_line = false,
+			next = "<M-]>",
+			prev = "<M-[>",
+			dismiss = "<C-]>",
+		},
+	},
+	filetypes = {
+		yaml = false,
+		markdown = false,
+		help = false,
+		gitcommit = false,
+		gitrebase = false,
+		hgcommit = false,
+		svn = false,
+		cvs = false,
+		["."] = false,
+	},
+	copilot_node_command = "node", -- Node.js version must be > 18.x
+	server_opts_overrides = {},
+})
+-- set colorscheme for overall vim
+require("bufferline").setup({
+	options = {
+		themable = true,
+		indicator = { style = "icon", icon = "▎" },
+		separator_style = { "", "" },
+		buffer_close_icon = "",
+		modified_icon = "●",
+		close_icon = "",
+		left_trunc_marker = "",
+		right_trunc_marker = "",
+		max_name_length = 20,
+		max_prefix_length = 15, -- prefix used when a buffer is de-duplicated
+		tab_size = 22,
+		show_buffer_close_icons = true,
+		show_close_icon = true,
+		always_show_bufferline = true,
+	},
+})
+vim.cmd("AirlineTheme deus")
+vim.cmd("colorscheme onedark")
