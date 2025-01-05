daaa6d2a50c3e7f89c52438e4b71cb9a30086a6f
yawnbo <ownuniverse@yahoo.com>
2025-01-05 13:18:11 -0800

    tree toggle

--- .config/nvim/lua/opts.lua
+++ .config/nvim/lua/opts.lua
@@ -78,6 +78,12 @@ vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left wind
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+vim.keymap.set("n", "<leader>o", "<cmd>NvimTreeToggle<CR>", {
+	desc = "Toggle filetree",
+	noremap = true,
+	silent = true,
+})
+
 --vim.keymap.set("n", "leader-t",
 --highlight yanks
 vim.api.nvim_create_autocmd("TextYankPost", {
