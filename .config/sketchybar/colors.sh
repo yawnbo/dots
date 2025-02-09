5da590272e3143b629f2f633ca75e07721b95238
yawnbo <ownuniverse@yahoo.com>
2025-02-08 22:09:59 -0800

    sketchybar

---
+++ .config/sketchybar/colors.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+### Sonokai
+# export BLACK=0xff181819
+# export WHITE=0xffe2e2e3
+# export RED=0xfffc5d7c
+# export GREEN=0xff9ed072
+# export BLUE=0xff76cce0
+# export YELLOW=0xffe7c664
+# export ORANGE=0xfff39660
+# export MAGENTA=0xffb39df3
+# export GREY=0xff7f8490
+# export TRANSPARENT=0x00000000
+# export BG0=0xff2c2e34
+# export BG1=0xff363944
+# export BG2=0xff414550
+
+### Gruvbox (kitty theme)
+export BLACK=0xcc3c3836
+export WHITE=0xccebdbb2
+export RED=0xccfb4934
+export GREEN=0xccb8bb26
+export BLUE=0xcc83a598
+export YELLOW=0xccd79921
+export ORANGE=0xccfe8019
+export MAGENTA=0xccd3869b
+export GREY=0xcca89984
+export TRANSPARENT=0x00000000
+export BG0=0xcc32302f
+export BG1=0xcc3c3836
+export BG2=0xcc504945
+
+### Gruvbox (original) dark soft
+# export BLACK=0xff282828
+# export WHITE=0xffebdbb2
+# export RED=0xfffb4934
+# export GREEN=0xffb8bb26
+# export BLUE=0xff83a598
+# export YELLOW=0xffd79921
+# export ORANGE=0xfffe8019
+# export MAGENTA=0xffd3869b
+# export GREY=0xffa89984
+# export TRANSPARENT=0x00000000
+# export BG0=0xff1d2021
+# export BG1=0xff3c3836
+# export BG2=0xff504945
+
+### Catppuccin
+# export BLACK=0xff181926
+# export WHITE=0xffcad3f5
+# export RED=0xffed8796
+# export GREEN=0xffa6da95
+# export BLUE=0xff8aadf4
+# export YELLOW=0xffeed49f
+# export ORANGE=0xfff5a97f
+# export MAGENTA=0xffc6a0f6
+# export GREY=0xff939ab7
+# export TRANSPARENT=0x00000000
+# export BG0=0xff1e1e2e
+# export BG1=0x603c3e4f
+# export BG2=0x60494d64
+
+# General bar colors
+export BAR_COLOR=$BG0
+export BAR_BORDER_COLOR=$BG2
+export BACKGROUND_1=$BG1
+export BACKGROUND_2=$BG2
+export ICON_COLOR=$WHITE # Color of all icons
+export LABEL_COLOR=$WHITE # Color of all labels
+export POPUP_BACKGROUND_COLOR=$BAR_COLOR
+export POPUP_BORDER_COLOR=$WHITE
+export SHADOW_COLOR=$BLACK
