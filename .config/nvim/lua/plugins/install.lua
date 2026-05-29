49a22e6267f9d797c741c327d14305453620a27e
Yan <yan@kovari.com>
2026-05-28 17:30:14 -0700

    devcontainer stuff

--- .config/nvim/lua/plugins/install.lua
+++ .config/nvim/lua/plugins/install.lua
@@ -850,7 +850,7 @@ require("lazy").setup({
 						workingDirectory = { mode = "auto" },
 					},
 				},
-				gopls = {},
+				-- gopls = {},
 				pyright = {},
 				jdtls = {},
 				ts_ls = {},
--- .zshrc
+++ .zshrc
@@ -193,4 +193,4 @@ deploy() {
 }
 
 eval "$(starship init zsh)"
-eval "$(zoxide init zsh)"
+# eval "$(zoxide init zsh)"
