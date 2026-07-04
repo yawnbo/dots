e0451ddc540433f07341d44dcbf83db079b79b11
yan <main@yawnbo.com>
2026-07-03 23:35:18 -0700

    osc52 clipboards

--- .config/nvim/lua/opts.lua
+++ .config/nvim/lua/opts.lua
@@ -124,6 +124,21 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 })
 
 -- sync clipboard
+if vim.env.SSH_CONNECTION then
+	local osc52 = require("vim.ui.clipboard.osc52")
+	vim.g.clipboard = {
+		name = "OSC 52",
+		copy = {
+			["+"] = osc52.copy("+"),
+			["*"] = osc52.copy("*"),
+		},
+		paste = {
+			["+"] = osc52.paste("+"),
+			["*"] = osc52.paste("*"),
+		},
+	}
+end
+
 vim.schedule(function()
 	vim.opt.clipboard = "unnamedplus"
 end)
